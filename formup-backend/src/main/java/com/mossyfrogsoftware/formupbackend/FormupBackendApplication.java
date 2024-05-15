337465721a019ded5b2cf1b2c517a5342200d12b
David <davidayresbyrne@gmail.com>
2024-05-15 21:25:33 +0100

    initial spring boot with dependencies commit

---
+++ formup-backend/src/main/java/com/mossyfrogsoftware/formupbackend/FormupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mossyfrogsoftware.formupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormupBackendApplication.class, args);
+	}
+
+}
---
+++ formup-backend/src/test/java/com/mossyfrogsoftware/formupbackend/FormupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mossyfrogsoftware.formupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
